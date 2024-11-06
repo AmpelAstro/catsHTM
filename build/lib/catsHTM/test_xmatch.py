dd3c22250c962aadced89bac027517f766018d24
Jakob van Santen <jvansanten@gmail.com>
2024-11-06 14:58:20 +0100

    Remove build artifacts

--- build/lib/catsHTM/test_xmatch.py
+++
@@ -1,7 +0,0 @@
-path='../../data'
-
-import catsHTM
-
-#catsHTM.xmatch_2cats('FIRST','AKARI',catalogs_dir=path)
-
-catsHTM.xmatch_2cats('FIRST','TMASSSxsc',catalogs_dir=path,Search_radius=10, save_results=True)
